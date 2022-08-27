4360d8d09b3e1f8e81491638436f32ef4588d79b
Leonid Rotarenco <leonid@bend.md>
2022-08-27 11:59:31 +0300

    remove prisma from example

--- index.ts
+++ index.ts
@@ -1,15 +1,2 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient();
-
-(async () => {
-  const file = await prisma.file.findUnique({
-    where: {
-      id: 5,
-    }
-  });
-
-
-  file  //// hover on it and click on type File
-  // I view File that is global js object, instead of file from prisma
-})()
+import {fn} from './t';
+const b = fn()
---
+++ t.ts
@@ -0,0 +1,7 @@
+export type File = {
+  bbn: number;
+}
+
+export const fn = (): File => {
+  return null as any;
+}
